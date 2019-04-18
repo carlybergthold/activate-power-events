250f79ef98f00c96b1359ac56124cd0de5d48183
Carly Bergthold <carlybergthold@gmail.com>
2019-04-18 11:49:50 -0500

    refactored code to be more DRY

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
-// Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
 
-document.querySelector("#activate-flight").addEventListener("click", () => {
-    const flightSection = document.querySelector('#flight');
-    flightSection.classList.toggle("enabled");
-});
+//event listener to toggle powers on the first three buttons
+document.addEventListener('click', () => {
 
-//Activate Mindreading button
-document.querySelector("#activate-mindreading").addEventListener("click", () => {
-    const mindreading = document.querySelector('#mindreading');
-    mindreading.classList.toggle("enabled");
-});
+    let section = event.target.id.split("-");
+    let id = section[1];
+    const flightSection = document.querySelector(`#${id}`).classList.toggle("enabled");;
 
-//Activate xray button
-document.querySelector("#activate-xray").addEventListener("click", () => {
-    const xray = document.querySelector('#xray');
-    xray.classList.toggle("enabled");
-});
+})
+
 
 //Activate All button
 document.querySelector("#activate-all").addEventListener("click", () => {
@@ -32,6 +24,26 @@ document.querySelector("#deactivate-all").addEventListener("click", () => {
         element.classList.remove("enabled");
     });
 });
+
+
+// Original code for the first three buttons
+
+// document.querySelector("#activate-flight").addEventListener("click", () => {
+//     const flightSection = document.querySelector('#flight');
+//     flightSection.classList.toggle("enabled");
+// });
+
+// //Activate Mindreading button
+// document.querySelector("#activate-mindreading").addEventListener("click", () => {
+//     const mindreading = document.querySelector('#mindreading');
+//     mindreading.classList.toggle("enabled");
+// });
+
+// //Activate xray button
+// document.querySelector("#activate-xray").addEventListener("click", () => {
+//     const xray = document.querySelector('#xray');
+//     xray.classList.toggle("enabled");
+// });
 
 
 
